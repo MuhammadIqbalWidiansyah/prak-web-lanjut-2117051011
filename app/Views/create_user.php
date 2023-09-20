d09e1083125d3db2cf599488bac5666d04049621
Muhammad Iqbal Widiansyah <muhiqbalmiw@gmail.com>
2023-09-20 16:06:26 +0700

    Tugas 3

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -7,17 +7,19 @@
     <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
 </head>
 <body>
-    <form action="<?=base_url('/user/store')?>" method="post">
-        <label for="nama">Nama:</label>
-        <input type="text" id="nama" name="nama" required><br>
-
-        <label for="nama">NPM:</label>
-        <input type="text" id="npm" name="npm" required><br>
-
-        <label for="nama">Kelas:</label>
-        <input type="text" id="kelas" name="kelas" required><br>
-        
-        <button type="submit">Submit</button>
-    </form>
+    <div class="container">
+        <div class="form-container" id="login-form">
+            <h1>Data Diri</h1>
+            <form action="<?=base_url('/user/store')?>" method="post">
+                <label for="nama">Nama</label>
+                <input type="text" id="nama" name="nama" required>
+                <label for="npm">NPM</label>
+                <input type="text" id="npm" name="npm" required>
+                <label for="kelas">Kelas</label>
+                <input type="text" id="kelas" name="kelas" required>
+                <button type="submit">Kirim</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
