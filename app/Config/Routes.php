3f73052d8e6483c47ea9a50bdc805a318cf6b695
Muhammad Iqbal Widiansyah <muhiqbalmiw@gmail.com>
2023-09-20 15:05:17 +0700

    Praktikum 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,4 @@ $routes->get('/', 'Home::index');
 $routes->get('/user/profile', [UserController::class, 'profile']);
 $routes->get('/user/create', [UserController::class, 'create']);
 $routes->post('/user/store', [UserController::class, 'store']);
-// $routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+// $routes->get('/user/profile', 'Home::profile');
